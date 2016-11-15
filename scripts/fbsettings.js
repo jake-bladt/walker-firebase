f6081cda6d5eb76221b8792bf3f28fe3739a9f62
Jake Bladt <jake@jakebladt.com>
2016-11-14 23:15:39 -0500

    config->firebaseConfig

--- scripts/fbsettings.js
+++ scripts/fbsettings.js
@@ -1,13 +1,10 @@
+// Initialize Firebase
+var firebaseConfig = {
+  apiKey: "AIzaSyDE4EuRBm56SDJXxJS9omvsIDL5yFhQkck",
+  authDomain: "walker-firebase.firebaseapp.com",
+  databaseURL: "https://walker-firebase.firebaseio.com",
+  storageBucket: "walker-firebase.appspot.com",
+  messagingSenderId: "545290483394"
+};
 
-<script src="https://www.gstatic.com/firebasejs/3.6.0/firebase.js"></script>
-<script>
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyDE4EuRBm56SDJXxJS9omvsIDL5yFhQkck",
-    authDomain: "walker-firebase.firebaseapp.com",
-    databaseURL: "https://walker-firebase.firebaseio.com",
-    storageBucket: "walker-firebase.appspot.com",
-    messagingSenderId: "545290483394"
-  };
-  firebase.initializeApp(config);
-</script>
+firebase.initializeApp(firebaseConfig);
