13d1a1564aefedc691daead0d8ed4810e97e957e
Jake Bladt <jake@jakebladt.com>
2016-11-19 22:02:10 -0500

    +)

--- webclient/scripts/walker.js
+++ webclient/scripts/walker.js
@@ -34,7 +34,7 @@ var walker = (function($, fbase, hb) {
       var provider = new fbase.auth.GoogleAuthProvider();
       provider.addScope('https://www.googleapis.com/auth/plus.login');
       fbase.auth().signInWithPopup(provider).then(function(result) { 
-        console.log("user: ", JSON.stringify(result.user);
+        console.log("user: ", JSON.stringify(result.user));
         this.currentUser = result.user;
       }).catch(function(error) {
         console.log("error: ", error);
