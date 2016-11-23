b2e0efcc0499521dd9c7accbf0ba8b05e3c696a4
Jake Bladt <jake@jakebladt.com>
2016-11-23 00:53:15 -0500

    more logging

--- webclient/scripts/walker.js
+++ webclient/scripts/walker.js
@@ -15,6 +15,7 @@ var walker = (function($, fbase, hb) {
 
     assignUser: function(user) {
       this.viewModel.currentUser = user;
+      console.log(this.viewModel.currentUser);
       var usersRef = this.database.ref('users/' + user.uid);
       usersRef.once('value', function(snapshot) {
         if(snapshot.val() === null) {
