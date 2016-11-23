d500e1566f29bdbd4e599464b926e378d5da1a0f
Jake Bladt <jake@jakebladt.com>
2016-11-23 00:49:58 -0500

    -intermediate step

--- webclient/scripts/walker.js
+++ webclient/scripts/walker.js
@@ -15,8 +15,8 @@ var walker = (function($, fbase, hb) {
 
     assignUser: function(user) {
       this.viewModel.currentUser = user;
-      usersRef = this.database.ref('users');
-      usersRef.child(user.uid).once('value', function(snapshot) {
+      usersRef = this.database.ref('users/' + user.uid);
+      usersRef.once('value', function(snapshot) {
         if(snapshot.val() === null) {
           usersRef.put(user.uid, {
             uid: user.uid
