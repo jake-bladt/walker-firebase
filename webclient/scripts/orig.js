64f97d94ebc1dc46af04526c44765867dc7d03ad
Jake Bladt <jake@jakebladt.com>
2016-11-22 23:50:18 -0500

    orig<->walker

--- webclient/scripts/orig.js
+++ webclient/scripts/orig.js
@@ -13,15 +13,44 @@ var walker = (function($, fbase, hb) {
   return {
     database: fbase.database(),
 
-    loginViaGoogle: function() {
+    assignUser: function(user) {
+      this.currentUser = user;
+      console.log(user);
+      usersRef = this.database.child('users');
+      usersRef.child(user.uid).once('value', function(snapshot) {
+        if(snapshot.val() === null) {
+          usersRef.put(user.uid, {
+            uid: user.uid
+          });
+        }
+      })
+    },
+
+    watchForUpdates: function() {
+      if(this.viewModel.currentUser) {
+        var stepsCountsRef = this.database.ref('steps-data/' + currentUser);
+        stepsCountsRef.on("value", function(snapshot) {
+          console.log(JSON.stringify(snapshot));
+        });
+      }
+    },
+
+    loginViaGoogle: (function() {
       var provider = new fbase.auth.GoogleAuthProvider();
       provider.addScope('https://www.googleapis.com/auth/plus.login');
       fbase.auth().signInWithPopup(provider).then(function(result) { 
         this.assignUser(result.user);
       }).catch(function(error) {
         console.log("error: ", error);
+      })
+    }),
+
+    updateUI: function() {
+      if(this.viewModel.currentUser) {
+        // Update UI
       }
     }
+  }
 })(jQuery, firebase, Handlebars);
 
 walker.viewModel = {
@@ -35,6 +64,7 @@ $(document).ready(function() {
   $('#loginLink').click(function(e) {
     e.preventDefault();
     walker.loginViaGoogle();
+    walker.updateUI();
     console.log(walker.viewModel);
   });
 });
--- webclient/scripts/walker.js
+++ webclient/scripts/walker.js
@@ -13,44 +13,15 @@ var walker = (function($, fbase, hb) {
   return {
     database: fbase.database(),
 
-    assignUser: function(user) {
-      this.currentUser = user;
-      console.log(user);
-      usersRef = this.database.child('users');
-      usersRef.child(user.uid).once('value', function(snapshot) {
-        if(snapshot.val() === null) {
-          usersRef.put(user.uid, {
-            uid: user.uid
-          });
-        }
-      })
-    },
-
-    watchForUpdates: function() {
-      if(this.viewModel.currentUser) {
-        var stepsCountsRef = this.database.ref('steps-data/' + currentUser);
-        stepsCountsRef.on("value", function(snapshot) {
-          console.log(JSON.stringify(snapshot));
-        });
-      }
-    },
-
-    loginViaGoogle: (function() {
+    loginViaGoogle: function() {
       var provider = new fbase.auth.GoogleAuthProvider();
       provider.addScope('https://www.googleapis.com/auth/plus.login');
       fbase.auth().signInWithPopup(provider).then(function(result) { 
         this.assignUser(result.user);
       }).catch(function(error) {
         console.log("error: ", error);
-      })
-    }),
-
-    updateUI: function() {
-      if(this.viewModel.currentUser) {
-        // Update UI
       }
     }
-  }
 })(jQuery, firebase, Handlebars);
 
 walker.viewModel = {
@@ -64,7 +35,6 @@ $(document).ready(function() {
   $('#loginLink').click(function(e) {
     e.preventDefault();
     walker.loginViaGoogle();
-    walker.updateUI();
     console.log(walker.viewModel);
   });
 });
